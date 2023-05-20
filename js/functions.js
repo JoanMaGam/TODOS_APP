6014f50b02a981ccf95651df5f19ffab222809f2
Joan Marti <joanmartigamez@gmail.com [core]>
2023-05-21 01:21:03 +0200

    funcion printOneTask creada y estilos de prioridad creados con clases

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,34 @@
+const ulTask = document.querySelector('#ulTask');
+ulTask.className = 'row gap-1 bg-dark p-0 m-0';
+
 function printTasks(pList, pDom) {
-    pList.innerHTML = '';
+    pDom.innerHTML = '';
     pList.forEach(task => printOneTask(task, pDom));
 }
 
-function printOneTask(pTask, pDom) {
+{/* <li class="liTaskCSS">
+    <p id="task" class="pCSS">Lorem ent incidunt corrupti quos nesciunt.</p>
+    <button id="btnDelete" class="btnCSS">Eliminar</button>
+</li>  */}
 
+function printOneTask(pTask, pDom) {
+    console.log(pTask.title);
+    const li = document.createElement('li');
+    li.classList.add('liTaskCSS');
+
+    const p = document.createElement('p');
+    p.id = 'task';
+    p.classList.add('pCSS');
+    p.textContent = pTask.title;
+
+
+    const btnDel = document.createElement('button');
+    btnDel.id = 'btdDelete';
+    btnDel.classList.add('btnCSS');
+    btnDel.textContent = 'Eliminar';
+
+    li.append(p, btnDel);
+
+    pDom.appendChild(li);
 
 }
