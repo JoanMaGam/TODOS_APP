9532d57763c4b53d7b6672822e94a0a25c585f81
Joan Marti <joanmartigamez@gmail.com [core]>
2023-05-20 23:12:57 +0200

    array de datos creado

---
+++ js/data.js
@@ -0,0 +1,17 @@
+const taskList = [
+    {
+        'id': 1,
+        'title': 'Estudiar JavaScript',
+        'priority': 'urgente',
+    },
+    {
+        'id': 2,
+        'title': 'Buscar ofertas de empleo',
+        'priority': 'diaria',
+    },
+    {
+        'id': 3,
+        'title': 'Hacer un proyecto propio y subirlo a GitHub',
+        'priority': 'mensual',
+    }
+]
