f46b69c618b7e5597be190efd1836daf0493845f
Joan Marti <joanmartigamez@gmail.com [core]>
2023-05-21 02:54:20 +0200

    html modificado y funcion printOneTask modificada añadiendo los colores por prioridad

--- js/functions.js
+++ js/functions.js
@@ -10,9 +10,20 @@ function printTasks(pList, pDom) {
 </li>  */}
 
 function printOneTask(pTask, pDom) {
-    console.log(pTask.title);
+    const selectPriority = document.querySelector('#selectPriority');
+
     const li = document.createElement('li');
     li.classList.add('liTaskCSS');
+    // if (selectPriority.value === 'urgent') {
+    //     li.classList.add('.urgent')
+    // }
+    if (pTask.priority === 'urgente') {
+        li.classList.add('urgente');
+    } else if (pTask.priority === 'diaria') {
+        li.classList.add('diaria');
+    } else if (pTask.priority === 'mensual') {
+        li.classList.add('mensual');
+    }
 
     const p = document.createElement('p');
     p.id = 'task';
@@ -30,3 +41,28 @@ function printOneTask(pTask, pDom) {
     pDom.appendChild(li);
 
 }
+
+const selectPriority = document.querySelector('#selectPriority')
+// selectPriority.addEventListener('change', aplyPriority)
+console.log(selectPriority.value)
+// function aplyPriority(event) {
+//     let priority = event.target.value
+//     console.log(priority);
+//     switch (priority) {
+//         case 'defaultOption':
+//             return alert('Elige una prioridad')
+//             break;
+//         case 'urgent':
+//             return '.urgente'
+//             break;
+//         case 'daily':
+//             return '.diaria'
+//             break;
+//         case 'monthly':
+//             return '.mensual'
+//             break;
+//     }
+// }
+
+// let priority = aplyPriority(event)
+
