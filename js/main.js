f86c4ba03772aee05a875e698b5cb7b48e5bf707
Joan Marti <joanmartigamez@gmail.com [core]>
2023-05-21 15:20:56 +0200

    solucionado problema del defaultLi, cerrar hodfix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const ulTask = document.querySelector('#ulTask');
 ulTask.className = 'row gap-1 bg-dark p-0 m-0';
 
-printTasks(taskList, ulTask)
+const defaultLi = `<li id="noTasks" class="liTaskCSS bg-light">
+                        <p class="pCSS">NO HAY TAREAS</p>
+                   </li>`;
+
+printTasks(taskList, ulTask);
 
 const form = document.querySelector('#form');
 form.addEventListener('submit', getDataForm);
